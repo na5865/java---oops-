05b4703f56e0eed7a0f6920ab5906894a123fd98
na5865 <na5865@srmist.edu.in>
2026-02-17 22:31:20 +0530

    AddedUC3 Banner App

---
+++ UC3OOPSBannerApp.java
@@ -0,0 +1,19 @@
+public class UC3OOPSBannerApp {
+    public static void main(String[] args) {
+
+        String banner = String.join("\n",
+
+                " ####    ####    ######    ###### ",
+                "##    ## ##    ## ##    ##  ##     ",
+                "##    ## ##    ## ##    ##  ##     ",
+                "##    ## ##    ## ######    ###### ",
+                "##    ## ##    ## ##              ##",
+                "##    ## ##    ## ##              ##",
+                " ####    ####    ##          ###### "
+
+        );
+
+        System.out.println(banner);
+    }
+}
+
